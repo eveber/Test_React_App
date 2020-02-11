de7a3c49d2b3aade0ab2c442a61bbe9aa41c6032
eveber <eveber1981@gmail.com>
2020-02-11 11:45:42 +0200

    first commit of project

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img className='logo' src="https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto/gigs/868110/original/5518108cc4880baab2492895d8f9df0fd321baeb/create-2-custom-logo-designs-exclusive-hi-quality.png" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <div className='profile-image'>
+                {/* <img src='https://openimagedenoise.github.io/images/moana_16spp_oidn.jpg' /> */}
+            </div>
+            <div>AVA + Description</div>
+            <div>
+                My posts
+            <div>
+                New Post
+            </div>
+            <div>
+                <div>
+                    Post1
+            </div>
+            <div>
+                Post2
+            </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
