b87b92bf0ac8bd2e9556e195f9374c716ce2bf4b
eveber <eveber1981@gmail.com>
2020-02-13 21:45:17 +0200

    Added front-end of dialogs.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import m from './Dialogs.module.scss'
+import messageAvatar from '../images/post_avatar.jpg'
 
 const Dialogs = (props) => {
-    return (
+  return (
+    <div>
+      <label className={m.caption}>Диалоги</label>
       <div className={m.dialogsWrapper}>
-          Dialogs page...
+        <div className={m.dialogs}>
+          <div className={m.dialog}>
+            Петя
+          </div>
+          <div className={m.dialog}>
+            Коля
+          </div>
+        </div>
+
+        <div className={m.messages}>
+          <div className={m.message}>
+            <img src={messageAvatar} alt="" />
+            <p className={m.figure}>Yo man! How are You?</p>
+          </div>
+          <div className={m.message}>
+            <img src={messageAvatar} alt="" />
+            <p className={m.figure}>Hey! I'm fine!</p>
+          </div>
+        </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Dialogs;
