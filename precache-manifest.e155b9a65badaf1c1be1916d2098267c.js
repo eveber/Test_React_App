47a3fed0872e7636eeb9d930182480817b757a85
eveber <eveber1981@gmail.com>
2020-04-12 13:16:04 +0300

    Updates

---
+++ precache-manifest.e155b9a65badaf1c1be1916d2098267c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be50f2b36d4440ef4a2cfce24a9acef2",
+    "url": "/Test_React_App/index.html"
+  },
+  {
+    "revision": "16f617d96f229a9d0b15",
+    "url": "/Test_React_App/static/css/3.3cfffe9c.chunk.css"
+  },
+  {
+    "revision": "a268e1c62e0f661ba4ba",
+    "url": "/Test_React_App/static/css/4.02ef9c2d.chunk.css"
+  },
+  {
+    "revision": "552ebe35b826cb373b49",
+    "url": "/Test_React_App/static/css/5.b0c11e98.chunk.css"
+  },
+  {
+    "revision": "13e9cb6b18e399837b7d",
+    "url": "/Test_React_App/static/css/main.beca173e.chunk.css"
+  },
+  {
+    "revision": "7dba38ab4e4da7cfd884",
+    "url": "/Test_React_App/static/js/2.2d07b75a.chunk.js"
+  },
+  {
+    "revision": "be3eb4cbad5a344e9603583a731df32d",
+    "url": "/Test_React_App/static/js/2.2d07b75a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16f617d96f229a9d0b15",
+    "url": "/Test_React_App/static/js/3.8ef820b1.chunk.js"
+  },
+  {
+    "revision": "a268e1c62e0f661ba4ba",
+    "url": "/Test_React_App/static/js/4.f44ea48f.chunk.js"
+  },
+  {
+    "revision": "552ebe35b826cb373b49",
+    "url": "/Test_React_App/static/js/5.50f63b3b.chunk.js"
+  },
+  {
+    "revision": "13e9cb6b18e399837b7d",
+    "url": "/Test_React_App/static/js/main.586f75f3.chunk.js"
+  },
+  {
+    "revision": "cc61148bdc4b8ea8aa6d",
+    "url": "/Test_React_App/static/js/runtime-main.bc517457.js"
+  },
+  {
+    "revision": "52c8af4e03e823c2524913ecc323fe4e",
+    "url": "/Test_React_App/static/media/city_profile.52c8af4e.png"
+  },
+  {
+    "revision": "2a084c66626b26e0e8250868b7500021",
+    "url": "/Test_React_App/static/media/logo.2a084c66.png"
+  },
+  {
+    "revision": "9843bf80e3eec14762c37377ed29c4e3",
+    "url": "/Test_React_App/static/media/no_avatar.9843bf80.png"
+  },
+  {
+    "revision": "31182ef6cbe9963dc796de52c54662b5",
+    "url": "/Test_React_App/static/media/preloader2.31182ef6.svg"
+  },
+  {
+    "revision": "1a656e9a15746d5a90eb032c75f805a4",
+    "url": "/Test_React_App/static/media/sn-model.1a656e9a.png"
+  }
+]);
